f2ce017374fa9fad3893a4497c896779ebedcd0f
Евгений <evgenij.romanko@neoseo.com.ua>
2020-07-29 00:04:33 +0300

    add component modal

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
 import ContactsForm from './ContactsForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
+import Modal from './Modal';
 
 import '../../node_modules/modern-normalize/modern-normalize.css';
 
-export default class App extends Component {
+class App extends Component {
+  state = {
+    // message: '',
+    showModal: this.props.showModal,
+  };
+  static propTypes = {
+    message: propTypes.string,
+    showModal: propTypes.bool,
+  };
+
+  closeModal = () => {
+    // this.setState(({ showModal }) => ({
+    //   showModal: !showModal,
+    // }));
+    this.props.showModal = false;
+  };
+
   render() {
+    // const { showModal } = this.state;
+    const { message, showModal } = this.props;
+    console.log(showModal);
     return (
       <>
+        {message !== '' && showModal ? (
+          <Modal onClose={this.closeModal}>{message}</Modal>
+        ) : null}
+
         <h1>Phonebook</h1>
         <ContactsForm />
         <h2>Contact</h2>
@@ -20,3 +46,13 @@ export default class App extends Component {
     );
   }
 }
+
+const mapStateToProps = state => {
+  //console.log(state.message);
+  return {
+    message: state.contacts.message,
+    showModal: state.contacts.showModal,
+  };
+};
+
+export default connect(mapStateToProps)(App);
--- src/redux/contacts/contacts-reduser.js
+++ src/redux/contacts/contacts-reduser.js
@@ -9,6 +9,8 @@ const initialState = {
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ],
   filter: '',
+  message: '',
+  showModal: false,
 };
 
 const contactsReducer = createReducer(initialState, {
@@ -17,17 +19,25 @@ const contactsReducer = createReducer(initialState, {
       contact => contact.name === payload.name,
     );
     if (isAlreadyInContacts) {
-      alert(`${payload.name} is already in contacts`);
+      state.showModal = true;
+      state.message = `${payload.name} is already in contacts STATE`;
+      //console.log(state.message);
+
+      //alert(`${payload.name} is already in contacts`);
       return state;
     }
     if (!payload.name || !payload.number) {
-      alert('Fill in all the fields');
+      state.showModal = true;
+      state.message = 'Fill in all the fields';
+      //alert('Fill in all the fields');
       return state;
     }
 
     return {
       ...state,
       items: [...state.items, payload],
+      message: state.message,
+      showModal: state.showModal,
     };
   },
 
